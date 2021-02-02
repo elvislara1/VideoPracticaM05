ab54ee62cdc060aae4bfd2f41b615e9fe5eaf5e1
elvislara1 <elarac01@elpuig.xeill.net>
2021-02-02 21:23:23 +0100

    4rta modificacion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hola mundo");
         System.out.println("Hola mundo 2");
         System.out.println("Hola adios....");
+        System.out.println("Hola de nuevo");
     }
 }
